b280df4ce9104550cb60620bd2be05eb7928857f
KiarashSamsami <kiarash.samsami@gmail.com>
2024-05-24 13:46:28 -0600

    Problem 26

---
+++ p26.cpp
@@ -0,0 +1,18 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+ #include <numeric>
+using namespace std;
+
+
+
+int main(){
+
+
+    int i = 2; 
+    cout<<i<<endl;
+
+
+    return 0 ; 
+}
